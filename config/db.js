061cee82a73f6c15ab583ccd02c4a30ea23fceb9
Hamza Bashir <hmzabshr120@gmail.com>
2025-10-09 15:37:12 +0500

    Database connection established successfully

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 const db = async ()=>{
     const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log("Database connected successfully")
+    console.log("Database connected successfully to price tracking DB")
 }
 
 
